2697bddc2c7810311c415506099c639b7f74083b
Rangga <rangganurwahyu22@gmail.com>
2023-08-31 20:55:18 +0700

    Day 15

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require ('express')
 const app = express()
 const PORT = 8080
 const path = require("path")
+const bcrypt = require('bcrypt')
+const session = require('express-session')
+const flash = require('express-flash')
 
 // sequelize init
 const config = require('./src/config/config.json')
@@ -22,6 +25,22 @@ app.use(express.static(path.join(__dirname, "src/assets/image")))
 // //parsing data 
 app.use(express.urlencoded({ extended: false }))
 
+// setup flash
+app.use(flash())
+
+// setup session
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 2
+  },
+  store: new session.MemoryStore(),
+  saveUninitialized: true,
+  resave: false,
+  secret: 'secretValue'
+}))
+
 //routing 
 app.get('/', home)
 app.get('/blog', blog)
@@ -31,6 +50,13 @@ app.get('/contact', contact)
 app.get("/edit-blog/:id", editBlog)
 app.post("/update-blog/:id", updateBlog)
 app.get("/delete-blog/:id", deleteBlog)
+
+// login, register, logout 
+app.get("/register", formRegister)
+app.post("/register", addUser)
+app.get("/login", formLogin)
+app.post("/login", userLogin)
+app.get("/logout", logout)
 
 //local server 
 app.listen(PORT, () => {
@@ -43,8 +69,17 @@ async function home(req, res) {
 	try {
 		const query = `SELECT * FROM public."Users";`
 		let obj = await sequelize.query(query, { type: QueryTypes.SELECT })
-  
-	  res.render("index", { dataBlog: obj })
+
+		const data = obj.map((res) => ({
+			...res,
+			isLogin: req.session.isLogin,
+		}));
+
+		res.render("index", {
+			data,
+			isLogin: req.session.isLogin,
+			sigin: req.session.sigin,
+		});
 	} catch (err) {
 		console.log(err)
 	} 
@@ -52,13 +87,15 @@ async function home(req, res) {
 
 // blog
 function blog(req, res) {
-	res.render("blog");
+	res.render("blog", {
+			isLogin: req.session.isLogin,
+			sigin: req.session.sigin,
+		});
 }
 
 // add a new blog
 async function addBlog(req, res) {
 	try {
-		const { id } = req.params;
 		const {
 			name,
 			start_date,
@@ -194,7 +231,11 @@ async function blogDetail(req, res) {
 			...res,
 		}))
 
-      res.render('blog-detail', { blog: data[0] })
+      res.render('blog-detail', { 
+				blog: data[0],
+				isLogin: req.session.isLogin,
+				sigin: req.session.sigin,
+			});
 	} catch (err) {
 	  console.log(err)
   }
@@ -202,8 +243,11 @@ async function blogDetail(req, res) {
 
 //contact 
 function contact(req, res) {
-    res.render('contact')
-}
+    res.render('contact', {
+			isLogin: req.session.isLogin,
+			sigin: req.session.sigin,
+		});
+	}
 
 // Delete blog
 async function deleteBlog(req, res) {
@@ -213,6 +257,76 @@ async function deleteBlog(req, res) {
 		res.redirect("/")
 	} catch (err) {
 		console.log(err)
+	}
+}
+
+// Register
+function formRegister(req, res) {
+	res.render('register')
+  }
+  
+  async function addUser(req, res) {
+	try {
+	  const { name, email, password } = req.body
+	  const salt = 10
+
+	  await bcrypt.hash(password, salt, (err, hashPassword) => {
+		const query = `INSERT INTO sigins (name, email, password, "createdAt", "updatedAt") VALUES ('${name}', '${email}', '${hashPassword}', NOW(), NOW())`
+		sequelize.query(query)
+		res.redirect('login')
+	  })
+	} catch (err) {
+	  console.log(err)
+	}
+  }
+
+// Login 
+function formLogin(req, res) {
+	res.render('login')
+  }
+  
+  async function userLogin(req, res) {
+	try {
+	  const { email, password } = req.body
+	  const query = `SELECT * FROM sigins WHERE email = '${email}'`
+	  let obj = await sequelize.query(query, { type: QueryTypes.SELECT })
+  
+	  console.log(obj)
+  
+	  // Memeriksa apakah email belum terdaftar
+	  if(!obj.length) {
+		req.flash('danger', "Anda Belom Terdaftar!")
+		return res.redirect('/login')
+	  }
+  
+	  await bcrypt.compare(password, obj[0].password, (err, result) => {
+		if(!result) {
+		  req.flash('danger', 'Password Yang Anda Masukan Salah!')
+		  return res.redirect('/login')
+		} else {
+		  req.session.isLogin = true
+		  req.session.sigin = obj[0].name
+		  req.flash('success', 'Login Sukses')
+		  res.redirect('/')
+		}
+	  })
+	} catch (err) {
+	  console.log(err)
+	}
+  }
+  
+// logout
+function logout(req, res) {
+	if (req.session.isLogin) {
+		req.session.destroy((err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.redirect("/");
+			}
+		});
+	} else {
+		res.redirect("/");
 	}
 }
 
--- src/seeders/20230829080932-users.js
+++ src/seeders/20230829080932-users.js
@@ -3,23 +3,23 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return await queryInterface.bulkInsert(
-      "Users",
-      [
-        {
-          name : "Mobile Developer",
-          description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quisquam molestias modi suscipit earum deleniti numquam aliquid! Laborum, maxime saepe.",
-          image : "project-1.jpg",
-          start_date : "2023-08-01",
-          end_date : "2023-09-09",
-          javascript : true,
-          reactjs : true,
-          vuejs : true,
-          nodejs : true,
-        },
-      ],
-      {}
-    )
+    // return await queryInterface.bulkInsert(
+    //   "Users",
+    //   [
+    //     {
+    //       name : "Mobile Developer",
+    //       description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quisquam molestias modi suscipit earum deleniti numquam aliquid! Laborum, maxime saepe.",
+    //       image : "project-1.jpg",
+    //       start_date : "2023-08-01",
+    //       end_date : "2023-09-09",
+    //       javascript : true,
+    //       reactjs : true,
+    //       vuejs : true,
+    //       nodejs : true,
+    //     },
+    //   ],
+    //   {}
+    // )
   },
 
   async down (queryInterface, Sequelize) {
